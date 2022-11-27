ebdf84aff7057fe9663ff0eabb1933c470fa3f84
Ganesh, Mohan <mohanganesh@gmail.com>
2022-11-27 12:17:40 -0500

    removed the local reference

--- config/database.js
+++ config/database.js
@@ -4,8 +4,8 @@ module.exports = ({ env }) => ({
     default: {
       connector: "mongoose",
       settings: {
-        host: env("DATABASE_HOST", "127.0.0.1"),
-        srv: env.bool("DATABASE_SRV", false),
+        host: env("DATABASE_HOST"),
+        srv: env.bool("DATABASE_SRV"),
         port: env.int("DATABASE_PORT", 27017),
         database: env("DATABASE_NAME"),
         username: env("DATABASE_USERNAME"),
